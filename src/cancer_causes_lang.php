8a7e19448dd9c0bddb28f8a0b7fcd8b9f6399682
gezapeti <2914398+gezapeti@users.noreply.github.com>
2025-12-28 15:39:36 +0100

    Update cancer_causes_lang.php

--- src/cancer_causes_lang.php
+++ src/cancer_causes_lang.php
@@ -1,14 +1,14 @@
 <?
 
 $lang["cancer_causes_title"] = "Okok";
-$lang["cancer_causes_subtitle"] = "Mi okozza a rákot?";
+$lang["cancer_causes_subtitle"] = "Mi okozza a daganatot?";
 $lang["cancer_causes_intro"] = 
-"<p>A sejtekben kialakuló mutációk vagy hibák különböző tényezők következményei lehetnek. Egy gén hordozhat örökletes mutációt (amely növeli egy adott daganattípus kialakulásának kockázatát), de a környezeti tényezők és a hormonok is hozzájárulhatnak a daganat kialakulásához.</p>";
+"<p>A sejtekben kialakuló mutációk, vagyis genetikai hibák különböző tényezők következményei lehetnek. Egy gén hordozhat örökletes mutációt (amely növeli egy adott daganattípus kialakulásának kockázatát), de a környezeti tényezők és a hormonális változások is hozzájárulhatnak a daganat kialakulásához.</p>";
 
 $lang["cancer_causes_hereditary_t"] = "Örökletes tényezők";
 $lang["cancer_causes_hereditary_d"] = 
-"<p>Az embereknél ismertek olyan genetikai mutációk, amelyek fokozzák a rák kialakulásának kockázatát (például az emlőrák esetében a BRCA gén pozitivitás). Az állatok esetében ez a terület jelenleg is aktív kutatás tárgya. Számos mutáció hasonló az emberihez, mások viszont eltérőek. Jelenleg nem léteznek kereskedelmi tesztek, amelyekkel ezek a kockázati tényezők rutinszerűen vizsgálhatók lennének.</p>
-<p>Állatorvosi tapasztalatok szerint azonban bizonyos kutyafajták hajlamosabbak egyes daganattípusok kialakulására, mint mások (lásd: fajtahajlam). Éppen ezért a kutyatartók számára fontos, hogy tisztában legyenek az adott fajta jellegzetes betegségeivel. Minél tájékozottabbak vagyunk, annál gyorsabban észlelhetők az eltérések, és annál nagyobb az esély a gyógyulásra.</p>";
+"<p>Az embereknél ismertek olyan genetikai mutációk, amelyek fokozzák a rák kialakulásának kockázatát (például az emlőrák esetében a BRCA gén pozitivitás). Az állatok esetében ez a terület jelenleg is aktív kutatás tárgya. Számos mutáció hasonló az emberihez, mások viszont merőben eltérőek. Jelenleg nem léteznek kereskedelmi tesztek, amelyekkel ezek a kockázati tényezők rutinszerűen vizsgálhatók lennének.</p>
+<p>Állatorvosi tapasztalatok szerint azonban bizonyos kutyafajták hajlamosabbak egyes daganattípusok kialakulására, mint mások (lásd: fajtahajlam). Éppen ezért a kutyatartók számára fontos, hogy tisztában legyenek az adott fajta jellegzetes betegségeivel. Minél tájékozottabbak vagyunk, annál gyorsabban észlelhetők az eltérések, és annál nagyobb az esély a kezelésre.</p>";
 
 $lang["cancer_causes_environment_t"] = "Környezeti tényezők";
 
@@ -18,7 +18,7 @@ $lang["cancer_causes_virus_d"] =
 
 $lang["cancer_causes_chemicals_t"] = "Vegyi anyagok";
 $lang["cancer_causes_chemicals_d"] = 
-"<p>Az emberekhez hasonlóan a vegyi anyagok, például az azbeszt vagy a cigarettafüst is mutációkat idézhetnek elő a kutyák és macskák sejtjeiben. Az állatorvosi kutatások eddig csak kevés rákkeltő anyagot azonosítottak. Kimutatták azonban, hogy a dohányfüst három-négyszeresére növeli a limfóma és az orrüregi (sinonazális) daganatok kialakulásának kockázatát.</p>";
+"<p>Az emberekhez hasonlóan a vegyi anyagok, például az azbeszt vagy a cigarettafüst is mutációkat idézhetnek elő a kutyák és macskák sejtjeiben. Az állatorvosi kutatások eddig csak kevés, állatok számára rákkeltő anyagot azonosítottak. Kimutatták azonban, hogy a dohányfüst három-négyszeresére növeli a limfóma és az orrüregi (sinonazális) daganatok kialakulásának kockázatát.</p>";
 
 $lang["cancer_causes_biological_t"] = "Biológiai tényezők";
 
@@ -36,5 +36,6 @@ $lang["cancer_causes_references"] =
 <li>Henry CJ, Macy DW. The Etiology of Cancer. Withrow and MacEwen's Small Animal Clinical Oncology, 5. kiadás, 1. fejezet (15–26. oldal).</li>
 <li>​Butler MB, Bonnett BN, Rodney LP. Epidemiology and the Evidence-Based Medicine Approach. Withrow and MacEwen's Small Animal Clinical Oncology, 5. kiadás, 4. fejezet (72–79. oldal).</li>
 </ol>";
+
 
 ?>
