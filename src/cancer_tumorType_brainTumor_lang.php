1684a82b0ac98f81e5ad60a7248eb3f319c22bcd
gezapeti <2914398+gezapeti@users.noreply.github.com>
2026-01-01 22:41:08 +0100

    Update cancer_tumorType_brainTumor_lang.php

--- src/cancer_tumorType_brainTumor_lang.php
+++ src/cancer_tumorType_brainTumor_lang.php
@@ -3,58 +3,58 @@
 $lang["cancer_tumorType_brainTumor_summary_location"] = "Agy";
 $lang["cancer_tumorType_brainTumor_summary_behavior"] = "A típustól függ, többnyire lassú növekedésű";
 $lang["cancer_tumorType_brainTumor_summary_diagnosis"] = "Képalkotás: MRI vagy CT";
-$lang["cancer_tumorType_brainTumor_summary_treatment"] = "Sebészi beavatkozás, amelyet szükség esetén sugárterápia egészíthet ki";
+$lang["cancer_tumorType_brainTumor_summary_treatment"] = "Sebészi beavatkozás, amelyet lehetőség szerint sugárterápia egészíthet ki";
 $lang["cancer_tumorType_brainTumor_summary_prognosis"] = "Általában kedvezőtlen";
 
 $lang["cancer_tumorType_brainTumor_location"] = 
 "<p>Az agydaganatok többsége magából az agyszövetből vagy az agyi erekből ered.</p>
-<p>A leggyakoribb agydaganatok a meningeomák (az agyhártyából kiinduló daganatok) és a gliomák (az idegsejteket körülvevő támasztószövetből eredő daganatok).</p>
-<p>Az agyban található daganatok lehetnek más testtájakról származó áttétek is. Ilyen eset előfordulhat például emlő-, tüdő-, prosztatadaganatok, hemangioszarkóma (éreredetű daganat), rosszindulatú melanoma (bőrdaganat) vagy limfoszarkóma (nyirokcsomó-daganat) esetén. A daganatok kiindulhatnak az agy közvetlen környezetéből is, például az orrmelléküregekből, az orrüregből, idegekből, a pajzsmirigyből vagy a koponyacsontokból.</p> 
-<p>Az agyban leggyakrabban előforduló áttétes vagy közeli szövetekből betörő daganatok: hemangioszarkóma (éreredetű daganat, 29%), pajzsmirigydaganat (25%), limfoszarkóma (12%), áttétes karcinóma (11%), valamint az orrüregből az agyba terjedő daganatok.</p>
+<p>A leggyakoribb agytumorok a meningeomák (az agyhártyából kiinduló daganatok) és a gliomák (az idegsejteket körülvevő támasztószövetből eredő daganatok).</p>
+<p>Az agyban található szövetszaporulatok lehetnek más testtájakról származó áttétek is. Ilyen eset előfordulhat például emlő-, tüdő-, prosztatadaganatok, hemangioszarkóma (éreredetű tumor), rosszindulatú melanoma (bőrrák) vagy limfóma (nyirokcsomó-daganat) esetén. A daganatok kiindulhatnak az agy közvetlen környezetéből is, például az orrmelléküregekből, az orrüregből, idegekből, a pajzsmirigyből vagy a koponyacsontokból.</p> 
+<p>Az agyban leggyakrabban előforduló áttétes vagy közeli szövetekből betörő neoplazmák: hemangioszarkóma (éreredetű daganat, 29%), pajzsmirigydaganat (25%), limfóma (12%), áttétes karcinóma (11%), valamint az orrüregből az agyba terjedő tumorok.</p>
 <div class='inline-image'>
 <img src='/public/images/content/Cancer/Cancer_TumorType_BrainTumor_PhotoCTbrain.jpg'>
-<p>CT-felvétel, amelyen a nyíl agyi szövettömeget jelez</p>
+<p>CT-felvétel, amelyen a nyíl agyi szövetszaporulatot mutat</p>
 </div>";
 
 $lang["cancer_tumorType_brainTumor_characteristics"] = 
-"<h3>Viselkedés</h3>
+"<h3>Klinikai viselkedés</h3>
 <p>A klinikai tünetek változatosak, és attól függnek, hogy a daganat milyen hatást gyakorol a környező agyi területekre vagy távolabbi struktúrákra.</p> 
-<p>Mivel az agydaganat létfontosságú helyen helyezkedik el, még a jóindulatú daganatok is súlyos következményekkel járhatnak. A daganat növekedésével nyomást gyakorol az agyszövetre, ami károsodást, gyulladást és vérzést okozhat.</p> 
+<p>Mivel a szövetszaporulat létfontosságú helyen helyezkedik el, még a jóindulatú daganatok is súlyos következményekkel járhatnak. A tumor növekedésével nyomást gyakorol az agyszövetre, ami károsodást, gyulladást és vérzést okozhat.</p> 
 <p>A leggyakoribb idegrendszeri tünet kutyáknál az epilepsziás rohamok hirtelen megjelenése 5 évesnél idősebb korban (az esetek több mint 50%-ában). A rohamok között a kutya teljesen normális lehet. Előfordulhat látásvesztés is. Egyéb, kevésbé feltűnő tünetek közé tartozik az apátia és az étvágytalanság. Jellemző lehet még a viselkedés megváltozása, körben járkálás, a fej falhoz nyomása, illetve az egyik oldalról érkező ingerek figyelmen kívül hagyása.</p>
-<p>Ezek a tünetek lassan alakulnak ki, ezért előfordulhat, hogy a viselkedésváltozás már hónapokkal, sőt akár egy évvel a diagnózis előtt is jelen volt. Ezeket a tüneteket gyakran tévesen az öregedésnek tulajdonítják.</p>
+<p>Ezek a panaszok lassan alakulnak ki, ezért előfordulhat, hogy a viselkedésváltozás már hónapokkal, sőt akár egy évvel a diagnózis előtt is jelen volt. Ezeket a tüneteket gyakran tévesen az öregedésnek tulajdonítják.</p>
 
 <h3>Előfordulás</h3>
-<p>Az agydaganatok bármely életkorban, nemtől és fajtától függetlenül előfordulhatnak. Az átlagos megjelenési életkor 9 év (a meningeomák inkább idősebb kutyákban jellemzők).</p>
+<p>Az agydaganatok bármely életkorban, nemtől és fajtától függetlenül előfordulhatnak. Az átlagos megjelenési életkor 9 év (a meningeómák inkább idősebb kutyákban jellemzők).</p>
 
 <h3>Fajtahajlam</h3>
-<p>Nagyobb kockázatot figyeltek meg Boxereknél, Boston terriereknél, francia és angol bulldogoknál, valamint golden retrievereknél, más fajtákhoz képest. A hosszú orrú kutyáknál gyakoribbak a meningeomák, míg a rövid orrúaknál inkább a gliomák fordulnak elő.</p>";
+<p>Nagyobb kockázatot figyeltek meg boxereknél, boston terriereknél, francia és angol bulldogoknál, valamint golden retrievereknél, más fajtákhoz képest. A hosszú orrú kutyáknál gyakoribbak a meningeómák, míg a rövid orrúaknál inkább a gliómák fordulnak elő.</p>";
 
 $lang["cancer_tumorType_brainTumor_diagnosis"] = 
-"<p>Az agydaganat gyanúját a kutya jellemzői, kórelőzménye és neurológiai tünetei alapján lehet felvetni. Hasonló tüneteket azonban más betegségek is okozhatnak (pl. gyulladások, degeneratív idegrendszeri betegségek), ezért a pontos diagnózishoz képalkotó vizsgálat szükséges.</p> 
+"<p>Az agyi tumort a kutya fajta-jellemzői, kórelőzménye és neurológiai tünetei alapján lehet gyanítani. Hasonló panaszokat azonban más betegségek is okozhatnak (pl. gyulladások, degeneratív idegrendszeri betegségek), ezért a pontos diagnózishoz képalkotó vizsgálat szükséges, lehetőleg MRI.</p> 
 
 <h3>Klinikai és neurológiai vizsgálat</h3>
-<p>A neurológiai vizsgálat során ellenőrzik a nyaki fájdalmasságot és a mancsok helyzetének korrigálási képességét.</p>
+<p>A neurológiai vizsgálat során ellenőrzik a gerinc fájdalmasságot és a mancsok helyzetének korrigálási képességét.</p>
 
 <h3>Vér- és vizeletvizsgálat</h3>
-<p>A vér- és vizeletvizsgálat a kutya általános egészségi állapotáról és esetleges szervi eltéréseiről ad információt.</p>
+<p>A vér- és vizeletvizsgálat a kutya általános egészségi állapotáról és esetleges egyéb szervi eltéréseiről ad információt.</p>
 
 <h3>Ultrahang</h3>
 <p>Az agydaganatos kutyák 23%-ánál egyéb, az aggyal nem összefüggő daganat is jelen van. Ezek felderítésére javasolt a hasüreg ultrahangos vizsgálata.</p> 
 
 <h3>Röntgen</h3>
-<p>A tüdőben vagy más helyen lévő áttétek, illetve másodlagos daganatok kimutathatók röntgenfelvételekkel (több irányból készítve). Ritkán előfordulhat, hogy a meningeoma a koponyacsont helyi megvastagodását vagy akár csontpusztulást okoz.</p> 
+<p>A tüdőben vagy más helyen lévő áttétek, illetve másodlagos daganatok kimutathatók röntgenfelvételekkel (több irányból készítve). Ritkán előfordulhat, hogy a meningeóma a koponyacsont helyi megvastagodását vagy akár csontpusztulást okoz.</p> 
 <p>A koponya röntgenfelvétele agydaganat esetén általában nem ad hasznos információt.</p>
 
 <h3>MRI</h3>
-<p>Az MRI az agydaganatok kimutatásának legjobb módszere. Segítségével különbséget lehet tenni meningeoma és glioma között: a meningeoma inkább eltolja az agyszövetet, egyenletesen veszi fel a kontrasztanyagot és jól körülhatárolható, míg a glioma beszűri a környező szöveteket, a kontrasztanyagot egyenetlenül veszi fel, és gyakran nincs éles határa. Természetesen kivételek előfordulhatnak. Egy vizsgálat szerint MRI alapján az esetek kb. 70%-ában megjósolható volt a daganat típusa. A végleges diagnózis azonban csak szövettani mintavétellel állítható fel, amit a biztonságos mintavételi hely meghatározása után végeznek.</p>   
+<p>Az MRI az agydaganatok kimutatásának legjobb módszere. Segítségével különbséget lehet tenni meningeóma és glióma között: a meningeóma inkább eltolja az agyszövetet, egyenletesen veszi fel a kontrasztanyagot és jól körülhatárolható, míg a glióma beszűri a környező szöveteket, a kontrasztanyagot egyenetlenül veszi fel, és gyakran nincs éles határa. Természetesen kivételek előfordulhatnak. Egy vizsgálat szerint MRI alapján az esetek kb. 70%-ában megjósolható volt a daganat típusa. A végleges diagnózis azonban csak szövettani mintavétellel állítható fel, amit a biztonságos mintavételi hely meghatározása után végeznek.</p>   
 
 <h3>Agy-gerincvelői folyadék (liquor)</h3>
-<p>Nem teljesen tisztázott, mennyire hasznos vagy szükséges a liquor vizsgálata agydaganatos kutyáknál. A liquor gyakran kóros, de nem ad specifikus információt a daganatról. Ha a daganat az agyi érfonatból (choroid plexusból) ered, a liquor vizsgálata segíthet megállapítani, hogy jó- vagy rosszindulatú-e. A liquor mintavétel előtt azonban mindig javasolt az MRI-vizsgálat, mivel a koponyaűri nyomásfokozódás esetén a nyomás hirtelen csökkenése a daganat elmozdulását, sőt akár halálos kimenetelt is okozhat.</p>
+<p>Nem teljesen tisztázott, mennyire hasznos vagy szükséges a liquor vizsgálata agydaganatos kutyáknál. A liquor gyakran kóros, de nem ad specifikus információt a daganatról. Ha a tumor az agyi érfonatból (choroid plexusból) ered, a liquor vizsgálata segíthet megállapítani, hogy jó- vagy rosszindulatú-e. A liquor mintavétel előtt azonban mindig javasolt az MRI-vizsgálat, mivel a koponyaűri nyomásfokozódás esetén a nyomás hirtelen csökkenése a daganat elmozdulását, sőt akár halálos kimenetelt is okozhat.</p>
 ";
 
 $lang["cancer_tumorType_brainTumor_treatment"] = 
 "<h3>Sebészet</h3>
-<p>A daganat eltávolíthatósága attól függ, hol helyezkedik el és milyen típusú. A meningeomák az agykéregben és a kisagyban könnyebben hozzáférhetők, mint az agytörzsben. A gliomák nehezebben operálhatók, mivel gyakran beszűrik a környező szöveteket, de ez mindig egyedi elbírálást igényel.  
+<p>A daganat eltávolíthatósága attól függ, hol helyezkedik el és milyen típusú. A meningeómák az agykéregben és a kisagyban könnyebben hozzáférhetők, mint az agytörzsben. A gliómák nehezebben operálhatók, mivel gyakran beszűrik a környező szöveteket, de ez mindig egyedi elbírálást igényel.  
 A sebészi eltávolítás után javasolt kiegészítő kezelésként sugárterápia alkalmazása.</p>
 
 <h3>Sugárterápia</h3>
@@ -68,17 +68,17 @@ A sztereotaxiás sugársebészet során egyetlen alkalommal, nagy pontossággal,
 <p>Az egyéb terápiás módszerekről (pl. teljes test melegítése, génterápia, brachyterápia) kevés információ áll rendelkezésre.</p>";
 
 $lang["cancer_tumorType_brainTumor_prognosis"] = 
-"<p>Kezelés vagy támogató terápia hiányában a prognózis kedvezőtlen. Mivel agydaganatokból ritkán vesznek szövettani mintát, kevés adat áll rendelkezésre az egyes típusok várható túléléséről. A túlélési idő 0 naptól akár 3 évig is terjedhet.</p>  
+"<p>Kezelés vagy támogató/palliatív terápia hiányában a prognózis kedvezőtlen. Mivel agydaganatokból ritkán vesznek szövettani mintát, kevés adat áll rendelkezésre az egyes típusok várható túléléséről. A túlélési idő 0 naptól akár 3 évig is terjedhet.</p>  
 
 <p>Pozitív prognózist befolyásoló tényezők:</p>
 <ul>
 <li>Egyetlen daganat jelenléte több helyett</li>
-<li>Korlátozott neurológiai eltérések</li>
+<li>Enyhe neurológiai eltérések</li>
 <li>Sugárkezelés, illetve műtét és sugárkezelés kombinációja</li>
 <li>A daganat helyzete: az agy elülső részén elhelyezkedő daganatok esetén hosszabb a túlélés, mint a hátsó részen lévőknél</li>
 </ul>
 
-<p>A meningeomák körülbelül fele agresszív viselkedést mutat, és magas a kiújulás kockázata.</p>
+<p>A meningeómák körülbelül fele agresszív viselkedést mutat, és magas a kiújulás kockázata.</p>
 ";
 
 $lang["cancer_tumorType_brainTumor_references"] = 
@@ -88,3 +88,4 @@ $lang["cancer_tumorType_brainTumor_references"] =
 </ol>";
 
 ?>
+
