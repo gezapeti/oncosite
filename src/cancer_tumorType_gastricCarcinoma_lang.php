9ac8a4addc7ad00a828324370909588408f54001
gezapeti <2914398+gezapeti@users.noreply.github.com>
2026-01-18 23:10:04 +0100

    Refactor gastric carcinoma language file

--- src/cancer_tumorType_gastricCarcinoma_lang.php
+++ src/cancer_tumorType_gastricCarcinoma_lang.php
@@ -32,38 +32,38 @@ $lang["cancer_tumorType_gastricCarcinoma_diagnosis"] =
 "<p>Általában egy vérvétellel és röntgennel kezdődik a vizsgálat, de ezek nem elegendőek a diagnózishoz.</p>
 
 <h3>Vérvizsgálat</h3>
-<p>Általánosságban a vérvizsgálat nem ad specifikus bizonyítékot a gyomor adenokarcinómára. Segíthet bizonyos másodlagos folyamatok felderítésében, vagy a daganattípusok elkülönítésében.</p>
+<p>Általánosságban a vérvizsgálat nem ad specifikus bizonyítékot a gyomor adenokarcinómára. Segíthet bizonyos másodlagos folyamatok felderítésében, vagy egyéb daganattípusok elkülönítésében.</p>
 
 <h3>Röntgenvizsgálat</h3>
-<p>A gyomor röntgenfelvételén felfedezhető egy nem odaillő növedék. Gyomorfekély is kimutatható, ami gyakran előfordul adenokarcinómáknál. Esetenként a gyomor lassabban ürül (a kontrasztanyag lassabban mozog).</p>
+<p>A hasüreg röntgenfelvételén felfedezhető egy nem odaillő növedék. Gyomorfekély is kimutatható, ami gyakran előfordul adenokarcinómáknál. Esetenként a gyomor lassabban ürül (a kontrasztanyag lassabban halad tovább).</p>
 
 <h3>Ultrahang</h3>
-<p>Ultrahang segítségével a állatorvos nehezen hozzáférhető helyekről is mintát vehet tűvel. A daganat helyzete utalhat a daganattípusra. Az adenokarcinómák inkább a gyomorkimenet közelében fordulnak elő, míg a leiomyosarcomák a gyomor bejáratánál gyakoribbak.</p>
+<p>Ultrahang segítségével a állatorvos nehezen hozzáférhető helyekről is mintát vehet tűvel. A daganat helyzete utalhat a daganattípusra. Az adenokarcinómák inkább a gyomorkivezetés közelében fordulnak elő, míg a leiomioszarkómák a gyomor bejáratánál gyakoribbak.</p>
 
 <h3>Gasztroszkópia</h3>
-<p>Rugalmas cső bevezetésével a gyomorba az állatorvos vizualizálhatja a növedékeket és célzottan nagyobb mintákat vehet. Mivel a gyomordaganat felszíne felszínesen nekrózisos, gyulladt vagy fekélyes lehet, több nagyobb szövetmintára lehet szükség. Ez nem mindig oldható meg endoszkóppal, és műtétet igényelhet.</p>
+<p>Rugalmas cső bevezetésével a gyomorba az állatorvos vizualizálhatja a növedékeket és célzottan nagyobb mintákat vehet. Mivel a gyomordaganat felszíne sok esetben elhalt, gyulladt vagy fekélyes lehet, több nagyobb szövetmintára lehet szükség. Ez nem mindig oldható meg endoszkóppal, és hasi feltáró műtétre lehet szükség.</p>
 
-<h3>Exploratív műtét</h3>
-<p>Ez a biztos módja annak, hogy jó mintát kapjunk a daganatról.</p>
+<h3>Exploratív (feltáró) műtét</h3>
+<p>Ez a biztos módja annak, hogy jó mintát kapjunk a szövetszaporulatból.</p>
 
-<h3>Histológia</h3>
-<p>Ezeknek a vizsgálatoknak köszönhetően meghatározható, hogy a daganat rosszindulatú vagy nagyon agresszív-e. Ez segít eldönteni, szükséges-e további kezelés.</p>
+<h3>Körszövettan</h3>
+<p>Ezeknek a vizsgálatoknak köszönhetően meghatározható, hogy a daganat rosszindulatú, illetve klinikailag nagyon agresszív-e. Ez segít eldönteni, hogy szükséges-e további kezelés.</p>
 
-<p>Ha GIST (gastrointestinalis stromalis tumor) gyanúja áll fenn, CD117 festés történik a megerősítéshez. Ez egy ritka daganat, amely a gyomor-bélrendszer támasztószövetéből indul ki.</p>";
+<p>Ha GIST (gasztrointesztinális strómális tumor) gyanúja áll fenn, CD117 festés történik a megerősítéshez. Ez egy ritka daganat, amely a gyomor-bélrendszer támasztószövetéből indul ki.</p>";
 
 $lang["cancer_tumorType_gastricCarcinoma_treatment"] = 
 "<h3>Sebészet</h3>
-<p>Ha nincs áttét, és a daganat a gyomorra korlátozódik, a daganat eltávolítása elegendő lehet a gyógyuláshoz. A daganat mérete, helye és típusa befolyásolja, mennyire lehet radikálisan eltávolítani a daganatot. Minél több gyomorszövetet kell eltávolítani, annál nagyobb a hatás a kutya életminőségére.</p>
-<p>A műtét során az állatorvos ellenőrzi a hasüregi nyirokcsomókat, hogy felmérje, a daganat csak a gyomorra korlátozódik-e. Jóindulatú daganatok esetén a daganat eltávolítható anélkül, hogy sok egészséges szövetet kellene kivágni.</p>
+<p>Ha nincs áttét, és a daganat a gyomorra korlátozódik, a tumor eltávolítása elegendő lehet a gyógyuláshoz. A daganat mérete, helye és típusa befolyásolja, mennyire lehet radikálisan eltávolítani. Minél több gyomorszövetet kell kimetszeni, annál nagyobb a kutya életminőségére gyakorolt hatás.</p>
+<p>A műtét során az állatorvos ellenőrzi a hasüregi nyirokcsomókat, hogy felmérje, a daganat csak a gyomorra korlátozódik-e. Jóindulatú növedékek esetén a tumor eltávolítható anélkül, hogy sok egészséges szövetet kellene kivágni.</p>
 
 <h3>Sugárkezelés</h3>
-<p>Ritkán alkalmazzák, mivel a gyomor közeli szövetek nem tolerálják a sugárzást. Ha a daganat lymphoma, sugárkezeléssel kezelhető, mivel alacsonyabb dózis is hatékony, mint más daganattípusoknál. A gyomor lymphomái általában nem reagálnak jól a standard kemoterápiára.</p>
+<p>Ritkán alkalmazzák, mivel a gyomorhoz közeli szövetek nem tolerálják a sugárzást. Limfóma esetén sugárkezelés jól alkalmazható, mivel alacsonyabb dózis is hatékony, ellentétben más daganattípusokkal. A gyomor lymphomái általában nem reagálnak jól a standard kemoterápiára.</p>
 
 <h3>Támogató kezelés</h3>
 <p>Műtét után könnyen emészthető étrend segíthet a kutya gyorsabb felépülésében (kevesebb terheléssel).</p>";
 
 $lang["cancer_tumorType_gastricCarcinoma_prognosis"] = 
-"<p>Az előrejelzés általában kedvezőtlen. Még a műtét után is a legtöbb kutya a daganat kiújulása vagy áttétek miatt 6 hónapon belül elhalálozik. Ha azonban a daganat jóindulatú, a kutya teljesen meggyógyulhat a sebészeti eltávolítás után.</p>";
+"<p>A kórjóslat általában kedvezőtlen. Még a műtéttel együtt is a legtöbb kutya a daganat kiújulása vagy áttétek miatt 6 hónapon belül elhalálozik. Ha azonban a daganat jóindulatú, a kutya teljesen meggyógyulhat a sebészeti eltávolítás után.</p>";
 
 $lang["cancer_tumorType_gastricCarcinoma_references"] = 
 "<ol>
@@ -72,3 +72,4 @@ $lang["cancer_tumorType_gastricCarcinoma_references"] =
 
 
 ?>
+
