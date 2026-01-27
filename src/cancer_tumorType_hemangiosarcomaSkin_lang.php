5e0dd4c9417353f9087d0bde4986dda8d44da1fc
gezapeti <2914398+gezapeti@users.noreply.github.com>
2026-01-27 23:20:55 +0100

    Update Hungarian translations for hemangiosarcoma skin

--- src/cancer_tumorType_hemangiosarcomaSkin_lang.php
+++ src/cancer_tumorType_hemangiosarcomaSkin_lang.php
@@ -2,34 +2,34 @@
 
 $lang["cancer_tumorType_hemangiosarcomaSkin_summary_location"] = "Vérerek a bőrben";
 $lang["cancer_tumorType_hemangiosarcomaSkin_summary_behavior"] = "A daganat stádiumától függ";
-$lang["cancer_tumorType_hemangiosarcomaSkin_summary_diagnosis"] = "Hisztológia";
+$lang["cancer_tumorType_hemangiosarcomaSkin_summary_diagnosis"] = "Kórszövettan";
 $lang["cancer_tumorType_hemangiosarcomaSkin_summary_treatment"] = "Sebészeti beavatkozás és kemoterápia";
-$lang["cancer_tumorType_hemangiosarcomaSkin_summary_prognosis"] = "A klinikai stádiumhoz kötött";
+$lang["cancer_tumorType_hemangiosarcomaSkin_summary_prognosis"] = "A klinikai stádiumtól függ";
 
 $lang["cancer_tumorType_hemangiosarcomaSkin_location"] = 
-"<p>Ez a daganattípus a bőrben vagy a bőr alatti szövetekben helyezkedik el (különösen a világosabb szőrű területeken, a has bőrén vagy a kevésbé pigmentált előbőrön).</p>";
+"<p>Ez a daganattípus a bőrben vagy a bőr alatti szövetekben helyezkedik el (különösen a világosabb szőrű területeken, a has bőrén vagy a kevésbé pigmentált területeken).</p>";
 
 $lang["cancer_tumorType_hemangiosarcomaSkin_characteristics"] = 
-"<h3>Viselkedés</h3>
-<p>A hemangiosarcomák a vérerekből származó daganatok. A daganatot olyan sejtek alkotják, amelyek körvonalazzák a vérereket, és rákos sejtekké fejlődtek. Ez a daganat lehet bőralatti vagy szubkután, de megtalálható a szívben, a lépben és a májban is. Elhelyezkedéstől függően a hemangiosarcomák különböző viselkedést mutatnak (kevésbé vagy erősen malignus). A legnagyobb különbség a (szub)kután és az egyéb helyek között van. Ezért ezt a daganattípust az elhelyezkedése szerint tárgyaljuk. Általában a kis méretű, a bőrben jól körülhatárolt daganatoknak jobb a prognózisa, mint más daganatformáknak. A hemangiosarcomák az összes kutya daganat 2%-át képviselik.</p>
+"<h3>Klinikai viselkedés</h3>
+<p>A hemangioszarkomák a vérerekből származó daganatok. A daganatot olyan sejtek alkotják, amelyek körvonalazzák a vérereket, és rosszindulatú tumoros sejtekké fejlődtek. Ez a daganat előfordulhat bőrben vagy bőr alatti kötőszövetben, de megtalálható a szívben, a lépben és a májban is. Elhelyezkedéstől függően a hemangiosarcomák különböző viselkedést mutatnak (kevésbé vagy erősen rosszindulatú). A legnagyobb különbség a bőrbeni helyerődés és az egyéb lokációk között van. Ezért ezt a daganattípust az elhelyezkedése szerint tárgyaljuk. Általában a kis méretű, a bőrben jól körülhatárolt daganatoknak jobb a prognózisa, mint más daganatformáknak. A hemangiosarcomák az összes kutya daganat 2%-át képviselik.</p>
 
 <h3>Fajtára jellemző hajlam</h3>
-<p>Nagyobb kockázatot jelentett a Border collie, a Dalmatian, a Német juhász, a Golden retriever, a Magyar drótszőrű vizsla, a Labrador retriever, a tacskó és az agár esetében más fajtákhoz képest.</p>
+<p>Nagyobb kockázatot jelentett a Border collie, a Dalmata, a Német juhász, a Golden retriever, a Magyar drótszőrű vizsla, a Labrador retriever, a tacskó és az agár esetében más fajtákhoz képest.</p>
 
-<h3>Jelenségek</h3>
+<h3>Megjelenés</h3>
 <p>A felszíni daganatok kevésbé agresszívek, mint azok, amelyek a bőr alatti szöveteket vagy az izmokat is érintik. A II. és III. stádiumú daganatok jellemzően nagyok, rosszul körülhatároltak és gyakran véraláfutásra emlékeztetnek.</p>";
 
 $lang["cancer_tumorType_hemangiosarcomaSkin_diagnosis"] = 
-"<p>A diagnózishoz elengedhetetlen a biopszia (hisztopatológia). A daganat stádiumának értékelése a daganat mélysége alapján történik (I. stádium: dermisz, II. stádium: szubkután szövet, III. stádium: izomig terjed). Vérvizsgálat és mellkasröntgen javasolt a metasztázisok felmérésére.</p>";
+"<p>A diagnózishoz elengedhetetlen a biopszia (kórszövettan). A daganat stádiumának értékelése a daganat kiterjedésének mélysége alapján történik (I. stádium: dermisz, II. stádium: szubkután (bőr alatti) szövetek, III. stádium: izmokat is érinti). Vérvizsgálat és mellkasröntgen javasolt a metasztázisok felderítésére.</p>";
 
 $lang["cancer_tumorType_hemangiosarcomaSkin_treatment"] = 
-"<p>Nagy kiterjedésű sebészeti eltávolítás és kiegészítő kemoterápia javasolt. 
-A kemoterápia csökkentheti a daganat méretét, így lehetővé téve a sebészeti eltávolítást (ez az opció szükséges lehet, ha a daganat túl nagy a műtéti eltávolításhoz). Amikor a daganatot műtéttel eltávolítják, általában 1–2 cm-es határok (a daganat körüli szövet) elegendőek.</p>";
+"<p>Nagy kiterjedésű/invazív sebészeti eltávolítás és kiegészítő kemoterápia javasolt. 
+A kemoterápia csökkentheti a daganat méretét, így lehetővé téve a sebészeti eltávolítást (ez az opció szükséges lehet, ha a daganat túl nagy a műtéti eltávolításhoz). Amikor a szövetszaporulatot eltávolítják, általában 1–2 cm-es határok (a daganat körüli épnek látszó szövet) elegendőek.</p>";
 
 $lang["cancer_tumorType_hemangiosarcomaSkin_prognosis"] = 
-"<p>Az I. stádiumú bőri hemangiosarcoma: teljes sebészi eltávolítás 78%-ban lehetséges. Az esetek 30%-ában metasztázisok vannak jelen (a bőrben, bizonyos távolságra az elsődleges daganattól). Az átlagos túlélés 780 nap.</p>
-<p>A II. és III. stádiumban a daganatok csupán 23%-a távolítható el teljesen, mivel ezek nagyobbak és kevésbé jól körülhatároltak (= nehezebben elkülöníthetők a környező szövettől). II. és III. stádiumban az esetek 60%-ában metasztázis alakul ki a tüdőben, a helyi nyirokcsomókban és a bőr távoli részein. Ezeknél a kutyáknál a túlélési idő 172–307 nap. Ha a II. és III. stádiumot alaposan kezelik (műtét, esetleg kiegészítő radioterápia), a kiegészítő kemoterápia jelentősen növelheti a túlélést (szubkután hemangiosarcoma: 1189 nap; intramuszkuláris hemangiosarcoma: 273 nap).</p>
-<p>Minél mélyebbre hatol a daganat a szubkután szövetekbe, annál rosszabb a prognózis.</p>
+"<p>Az I. stádiumú bőr hemangiosarcoma: teljes sebészi eltávolítás a biztonsági zónákkal 78%-ban lehetséges. Az esetek 30%-ában áttétek már jelen vannak (a bőrben, bizonyos távolságra az elsődleges daganattól). Az átlagos túlélés 780 nap.</p>
+<p>A II. és III. stádiumban a daganatok csupán 23%-a távolítható el teljesen, mivel ezek nagyobbak és kevésbé jól körülhatároltak (= nehezebben elkülöníthetők a környező szövetektől). II. és III. stádiumban az esetek 60%-ában a áttétek alakulnak ki a tüdőben, a helyi nyirokcsomókban és a bőr távolabbi területein. Ezeknél a kutyáknál a túlélési idő 172–307 nap. A II. és III. stádiumú tumorokat komplexen kezelik (műtét, esetleg kiegészítő radioterápia), a kiegészítő kemoterápia jelentősen növelheti a túlélést (szubkután hemangiosarcoma: 1189 nap; intramuszkuláris hemangiosarcoma: 273 nap).</p>
+<p>Minél mélyebbre hatol a daganat a bőr alatti szövetekbe, annál rosszabb a kórjóslat.</p>
 <p>Szubkután vagy intramuszkuláris hemangiosarcomák esetén a lokális kiújulás több mint 20%-ban fordul elő, a metasztázisok pedig 42–75%-ban. A műtét és kemoterápia utáni medián túlélési idő kb. 1–1,5 év. Ha a kezelés csak műtéti, a medián túlélési idő 172–307 nap. Csak kemoterápia esetén a medián túlélési idő kb. 133 nap. Palliativ radioterápia esetén kb. 90 nap.</p>";
 
 $lang["cancer_tumorType_hemangiosarcomaSkin_references"] = 
@@ -40,5 +40,6 @@ $lang["cancer_tumorType_hemangiosarcomaSkin_references"] =
 <li>Denies S, Cicchelero L, de Rooster H, Daminet S, Polis I, Van de Maele I, Sanders NN.Immunological and angiogenic markers during metronomic temozolomide and cyclophosphamide in canine cancer patients. Veterinary Comparative Oncology 2016.</li>
 <li>Clifford C, Topics in Veterinary Oncology 2025</li>
 </ol>";
+
 
 ?>
