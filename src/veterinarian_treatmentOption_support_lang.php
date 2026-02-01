d3a5d4708bee0daa015954770b38298cb778a281
gezapeti <2914398+gezapeti@users.noreply.github.com>
2026-02-01 22:28:28 +0100

    Update veterinarian_treatmentOption_support_lang.php

--- src/veterinarian_treatmentOption_support_lang.php
+++ src/veterinarian_treatmentOption_support_lang.php
@@ -3,14 +3,15 @@
 $lang["veterinarian_treatment_support"] = 
 "<p>A háziállat kezelése során mindig a lehető legmagasabb életminőség biztosítása a cél. Ezt úgy érhetjük el, hogy minimalizáljuk a kezelés mellékhatásait, valamint a daganat növekedéséből és/vagy magából a kezelésből eredő fájdalmat vagy kellemetlenséget.</p>
  
-<p>Étvágycsökkenés esetén étvágygerjesztők adhatók. Emellett a táplálkozást mindig az állat egyedi igényeihez kell igazítani (például más betegségeket, mint a veseproblémákat is figyelembe véve), és teljes, kiegyensúlyozott étrendet kell biztosítani.</p>
+<p>Étvágycsökkenés esetén étvágynövelők adhatóak. Emellett a táplálkozást mindig az állat egyedi igényeihez kell igazítani (például más betegségeket, úgymint a veseproblémákat is figyelembe véve), és teljes, kiegyensúlyozott étrendet kell biztosítani.</p>
  
-<p>Magát a rákot, de a kezelést is okozhat (ideiglenes) fájdalmat, ami többféle fájdalomcsillapító alkalmazását teheti szükségessé. Részletes irányelvek állnak rendelkezésre a fájdalom helyes kezelésére (AAHA/AAFP fájdalom irányelvek).</p>
+<p>Maga a szövetszaporulat, de akár a kezelés is okozhat (ideiglenesen) fájdalmat, amely többféle fájdalomcsillapító alkalmazását teheti szükségessé. Részletes irányelvek állnak rendelkezésre a fájdalom helyes kezelésére (AAHA/AAFP fájdalomcsillapítás irányelvek).</p>
  
-<p>Lehetőség van arra is, hogy ne kezeljük a rákot, különösen akkor, ha nincs gyógyulási esély. Ilyenkor palliatív ellátás javasolt, a lehető legmagasabb életminőség eléréséhez.</p>
+<p>Lehetőség van arra is, hogy magát a daganatot ne kezeljük, különösen akkor, ha nincs gyógyulási esély. Ilyenkor palliatív ellátás javasolt, a lehető legmagasabb életminőség eléréséhez.</p>
  
-<p>Háziállatunk komfortérzetét és életminőségét javíthatjuk fájdalomcsillapítással, helyes táplálkozással és tüneti támogatással.</p>
+<p>Társállatunk komfortérzetét és életminőségét támogathatjuk fájdalomcsillapítással, helyes táplálkozással és tüneti kezeléssel.</p>
 
 <p><a href='/Me/support'>További információ ebben a témában</a></p>";
 
 ?>
+
