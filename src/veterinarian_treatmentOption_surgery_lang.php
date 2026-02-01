2bb79891fa5368814c1c324ad3eaf7d110ef51aa
gezapeti <2914398+gezapeti@users.noreply.github.com>
2026-02-01 12:55:25 +0100

    Update veterinarian_treatmentOption_surgery_lang.php

--- src/veterinarian_treatmentOption_surgery_lang.php
+++ src/veterinarian_treatmentOption_surgery_lang.php
@@ -1,27 +1,28 @@
 <?
 
 $lang["veterinarian_treatment_surgery"] = 
-"<p>A sebészeti beavatkozás általában az elsődleges választás olyan daganatok esetén, amelyek egy lokalizált területen helyezkednek el, és teljesen eltávolíthatók anélkül, hogy jelentősen veszélyeztetnénk az állat egészségét.</p>
-<p>A műtét célja, hogy a daganatot minden irányban elegendő biztonsági távolsággal vegyük körül, biztosítva, hogy az összes rákos szövet eltávolításra kerüljön. A daganat típusától függően szélesebb eltávolítási margóra lehet szükség: minél agresszívebb a daganat, illetve minél inkább beszűrődik a környező szövetekbe, annál nagyobb margót kell hagyni.</p>
+"<p>A sebészeti beavatkozás általában az elsődleges választás olyan daganatok esetén, amelyek egy lokalizált területen helyezkednek el, és teljesen eltávolíthatók anélkül, hogy azzal jelentősen veszélyeztetnénk az állat egészségét.</p>
+<p>A műtét célja, hogy a daganatot minden irányban elegendő biztonsági távolsággal, ép széllel vegyük körül, biztosítva, hogy az összes rákos szövet eltávolításra kerüljön. A daganat típusától függően szélesebb eltávolításra lehet szükség: minél agresszívebb a tumor, illetve minél inkább beszűri a környező szöveteket, annál nagyobb sebszélt kell tartani.</p>
 <div class='inline-image-50'>
 <img src='/public/images/content/Cancer/Cancer_TumorType_MastCellTumor_Carousel_2.jpg'>
-<p>A három daganatot a kis kör veszi körül, a nagy kör jelzi a daganat teljes eltávolításához szükséges margót</p>
+<p>A három daganatot a kis kör veszi körül, a nagy kör jelzi a daganat teljes eltávolításához szükséges sebszélt</p>
 </div>
-<p>A daganat sebészeti eltávolítása előtt az állat altatásban van. A műtét célja az összes rákos szövet eltávolítása. Néha azonban ez nem teljesen lehetséges, és maradhat rákos szövet, ami további kezelést tesz szükségessé. Elméletileg egy daganat részleges eltávolítása érzékenyebbé teheti azt más terápiákra, például kemoterápiára, sugárkezelésre vagy immunterápiára.</p>
-<p>A kiújult daganatokat általában nehezebb eltávolítani, mert az első műtét megváltoztathatja a szövetet, így a daganat a környező szövetekben szélesebb terjedést mutathat.</p>
-<p>Az összes eltávolított daganatot két okból kell histopatológiailag vizsgálni: a daganat típusának meghatározása és annak biztosítása érdekében, hogy a daganat teljesen eltávolításra került.</p>
+<p>A daganat sebészeti eltávolítása altatásban történik. A műtét célja az összes tumoros szövet eltávolítása. Időnként azonban ez nem teljesen lehetséges, és maradhat daganatszövet, ami kiújulással fenyeget, azért további kezelést igényel. Alkalmanként egy tumort a részleges műtéti eltávolítása érzékenyebbé tehet más terápiákra, például kemoterápiára, sugárkezelésre vagy immunterápiára.</p>
+<p>A kiújult daganatokat általában nehezebb eltávolítani, mert az első műtét megváltoztatja a szöveti szerkezetet, a tumor a környező szövetekben szélesebb terjedést mutathat.</p>
+<p>Az eltávolított daganatot minden esetben kórszövettani vizsgálatnak kell alávetni. A vizsgálat szükségességének két fő oka van: a daganat típusának meghatározása, valamint megbizonyosodni róla, hogy a tumorszövet teljes egészében eltávolításra került.</p>
 <ul>
 <li>Fontos ismerni a daganat stádiumát és típusát, hogy meghatározhassuk a következő lépéseket: a probléma teljesen megoldódott-e például egy jóindulatú daganat esetén, vagy rosszindulatú daganatról van-e szó, amelynél nagy a kiújulás esélye.</li>
-<li>A helyes daganattípus felismerése lehetővé teszi annak eldöntését, hogy szükséges-e további kezelés (például kemoterápia, sugárkezelés, immunterápia), illetve hogy az indokolt-e. Tartja a mondás: „ha érdemes eltávolítani a daganatot, érdemes histológiai diagnózist is készíteni.”</li>
-<li>Ha csak nagyon keskeny vagy tisztátlan margót lehet biztosítani, a következő lehetőségek maradnak:</li>
+<li>A helyes daganattípus felismerése lehetővé teszi annak eldöntését, hogy szükséges-e további kezelés (például kemoterápia, sugárkezelés, immunterápia), illetve hogy az indokolt-e. Egy mondás szerint: „ha érdemes tartjuk eltávolítani a daganatot, a kórszövettan alapján érdemes a diagnózist is felállítani.”</li>
+<li>Ha csak nagyon vékony vagy szennyezett sebszélt lehet biztosítani, a következő lehetőségek közül lehet választani:</li>
 <ul>
-<li>A daganat szoros megfigyelése (olyan daganattípusok esetén, ahol a kiújuláskor eltávolítható). Elsőre talán furcsának tűnik, de bizonyos daganatoknál beszámoltak arról, hogy a megmaradt rákos sejtek a kutya saját immunrendszere által elpusztulnak, és nem újulnak ki, még ha a margó nem is volt teljesen tiszta;</li>
-<li>Azonnal újabb sebészeti beavatkozás szélesebb margókkal az eredeti műtéti bemetszés körül;</li>
+<li>A műtéti terület szoros megfigyelése (olyan daganattípusok esetén, amelyek kiújuláskor eltávolíthatóak). Elsőre talán furcsának tűnik, de bizonyos tumorokról beszámoltak arról, hogy a megmaradt rákos sejtek a kutya saját immunrendszere által elpusztulnak, és nem feltétlenül újulnak ki, még ha a sebszél nem is volt teljesen tiszta;</li>
+<li>Azonnal újabb sebészeti beavatkozás szélesebb margókkal az eredeti műtéti terület körül;</li>
 <li>Adjuváns kemoterápia alkalmazása. Ez hasznos lehet olyan daganatoknál, amelyek bizonyos kemoterápiás szerekre jól reagálnak;</li>
 <li>Adjuváns sugárkezelés alkalmazása.</li>
 </ul>
 </ul>
-<p>A kutya mentális állapotát sem szabad alábecsülni: a (nagy) daganat sebészi eltávolítása után az állat megjelenése/arckifejezése jelentősen változhat. Ez a kutyát magát nem zavarja, de a gazdákat, járókelőket megijesztheti vagy kérdéseket vethet fel. 
-Általánosságban az állandó figyelem és szeretet csökkentheti a fájdalomérzetet mind emberekben, mind állatokban. Fontos elkerülni a kutyánál az unalmat, fáradtságot és izolációt, mert ezek csökkentik a fájdalomküszöböt. A rendszeres napirend és környezet, elegendő pihenés/alvás, környezeti gazdagítás, társaság, együttérzés és a kellemetlen környezet elkerülése jelentősen csökkentheti a rákkal kapcsolatos kellemetlenségeket.</p>";
+<p>A kutya mentális állapotát sem szabad figyelmen kívül hagyni: a (nagy) daganat sebészi eltávolítása után az állat megjelenése/arckifejezése jelentősen változhat. Ez a kutyát magát nem zavarja, de a gazdákat megrémítheti, a járókelők esetleg kérdésekkel terhelhetik a tulajdonost. 
+Általánosságban az állandó figyelem és szeretet csökkentheti a szenvedést, fájdalomérzetet mind emberekben, mind állatokban. Fontos elkerülni a tumorbeteg kutyánál az unalmat, elhanyagolást és izolációt, mert ezek csökkentik a fájdalomküszöböt. A rendszeres napirend és állandóság, elegendő pihenés/alvás, környezeti gazdagítás, társaság, együttérzés, és a kellemetlen ingerek elkerülése jelentősen csökkentheti a daganatos betegséggel kapcsolatos szenvedést.</p>";
 
 ?>
+
