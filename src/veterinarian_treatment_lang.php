76c17241404306437ac4a974c135c31e7a13532c
gezapeti <2914398+gezapeti@users.noreply.github.com>
2026-02-01 12:15:28 +0100

    Revise veterinarian treatment language content
    Updated treatment options and introductory text for clarity and accuracy.

--- src/veterinarian_treatment_lang.php
+++ src/veterinarian_treatment_lang.php
@@ -1,11 +1,11 @@
 <?
 
 $lang["veterinarian_treatment_title"] = "Kezelés";
-$lang["veterinarian_treatment_subtitle"] = "Gyógyítható a rák?";
+$lang["veterinarian_treatment_subtitle"] = "Gyógyítható e a rák?";
 $lang["veterinarian_treatment_intro"] =
-"<p>Az állatok számára különböző kezelési lehetőségek állnak rendelkezésre. A különböző daganattípusok eltérő kezelési megközelítést igényelhetnek. Emellett a helyzettől függően több kezelés kombinációja is szükséges lehet.</p>
-<p>Például helyi daganatkontroll elérésére műtét és/vagy sugárkezelés alkalmazható. A műtét lehetővé teszi a daganat eltávolítását kimetszéssel, míg a sugárkezelés a környező szövetek ráksejtjeit radioaktív vagy ionizáló sugarakkal célozza.</p>
-<p>A metasztatizált daganatok esetén kemoterápia és/vagy immunterápia alkalmazható. A kemoterápia erős gyógyszerekkel pusztítja el a ráksejteket, az immunterápia pedig a szervezet immunrendszerét aktiválva támadja a daganatsejteket.</p>
+"<p>Az állatok számára különböző kezelési lehetőségek állnak rendelkezésre. A különböző daganattípusok eltérő terápiás megközelítést igényelhetnek. Emellett a helyzettől függően több kezelés kombinációja is szükséges lehet.</p>
+<p>Például helyi daganatkontroll elérésére műtét és/vagy sugárkezelés alkalmazható. A műtét lehetővé teszi a daganat eltávolítását kimetszéssel, míg a sugárterápia a környező szövetek tumorsejtjeit radioaktív vagy ionizáló sugarakkal pusztítja.</p>
+<p>A metasztatizált/áttétes daganatok esetén kemoterápia és/vagy immunterápia alkalmazható. A kemoterápia erős gyógyszerekkel pusztítja el a neoplasztikus sejteket, az immunterápia pedig a szervezet immunrendszerét aktiválva támadja a daganatsejteket.</p>
 <p>Az alábbiakban a leggyakoribb kezelési lehetőségek általános áttekintését találja. A <a href='https://oncowaf.be/en/Cancer/tumorTypes' target='blank'>„Daganattípusok”</a> szekcióban részletesebb leírást találhat a kezelési lehetőségekről típusonként.</p>";
 
 $lang["veterinarian_treatment_treatmentOption_chemotherapy"] = "kemoterápia";
@@ -16,8 +16,9 @@ $lang["veterinarian_treatment_treatmentOption_immunotherapy"] = "immunterápia";
 $lang["veterinarian_treatment_treatmentOption_interventionalRadiology"] = "intervenciós radiológia";
 $lang["veterinarian_treatment_treatmentOption_photodynamicTherapy"] = "fotodinámiás terápia";
 $lang["veterinarian_treatment_treatmentOption_radiotherapy"] = "sugárkezelés";
-$lang["veterinarian_treatment_treatmentOption_support"] = "támogató kezelés";
+$lang["veterinarian_treatment_treatmentOption_support"] = "támogató/palliatív kezelés";
 $lang["veterinarian_treatment_treatmentOption_surgery"] = "műtét";
 $lang["veterinarian_treatment_treatmentOption_targetedTherapy"] = "célzott terápia";
 
 ?>
+
