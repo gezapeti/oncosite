dd7ccc3cb0fa7bedbda2a2f78019b2df7b384c73
gezapeti <2914398+gezapeti@users.noreply.github.com>
2026-02-15 23:28:44 +0100

    Update Hungarian language content for melanoma eye

--- src/cancer_tumorType_melanomaEye_lang.php
+++ src/cancer_tumorType_melanomaEye_lang.php
@@ -1,22 +1,22 @@
 <? 
 
-$lang["cancer_tumorType_melanomaEye_summary_location"] = "A szemben";
-$lang["cancer_tumorType_melanomaEye_summary_behavior"] = "A szem különböző részein eltérő viselkedés";
+$lang["cancer_tumorType_melanomaEye_summary_location"] = "A szemgolyó és környező szövetei";
+$lang["cancer_tumorType_melanomaEye_summary_behavior"] = "A szem különböző régióiban eltérő viselkedés";
 $lang["cancer_tumorType_melanomaEye_summary_diagnosis"] = "Szemvizsgálat, felszíni nyirokcsomók aspirációja, mellkasröntgen, hasi ultrahang";
 $lang["cancer_tumorType_melanomaEye_summary_treatment"] = "Sebészeti eltávolítás";
 $lang["cancer_tumorType_melanomaEye_summary_prognosis"] = "Jó";
 
 $lang["cancer_tumorType_melanomaEye_location"] = 
 "<p>A szem melanómái az íriszből vagy a szem erekkel gazdagon ellátott középső rétegéből (uvea) eredhetnek.</p>
-<p>Melanómákat a szem környékén is leírtak: a sclera vagy a szemhéj alatti kötőszövet melanocitáiból származó pigmentált csomók figyelhetők meg.</p>
-<p>Melanómák kialakulhatnak a szőrös bőrön, a talpakon, a körömágyban, a szájüregben, a gyomor-bélrendszerben, valamint a nyálkahártya és a bőr átmeneti területein (pl. ajkak, végbél). A daganat helyétől függően a melanómák kevésbé vagy súlyosabban rosszindulatú viselkedést mutathatnak.</p>";
+<p>Melanómákat a szem környékén is leírtak: a sclera vagy a szemhéj alatti kötőszövet melanocitáiból származó pigmentált szövetszaporulatok figyelhetők meg.</p>
+<p>Melanómák kialakulhatnak a szőrös bőrön, a talpakon, a körömágyban, a szájüregben, a gyomor-bélrendszerben, valamint a nyálkahártya és a bőr átmeneti területein (pl. ajkak, végbél). A tumor helyétől függően a melanómák kevésbé vagy súlyosabban rosszindulatú viselkedést mutathatnak.</p>";
 
 $lang["cancer_tumorType_melanomaEye_characteristics"] = 
-"<h3>Viselkedés</h3>
-<p>Írisz-melanóma: egy jól körülhatárolt, sötét, csomós képlet jelenik meg az íriszen.</p> 
-<p>Ciliaris test melanóma: sötét tömegként látható a szemben, torzíthatja a pupillát, és azon keresztül előemelkedhet.</p> 
-<p>Choroidális melanóma: a choroidát és a retinát érinti. Kezdetben nem tapasztalhatók látáskárosodásra vagy gyulladásra utaló jelek. Később azonban szövődmények jelentkezhetnek, mint például vakság, glaucoma, retina leválás.</p>
-<p>A szem környékén megjelenő melanómáknál előfordulhat a szaruhártya helyi behatolása, fokozott könnytermelés és enyhe nyálkahártya-irritáció. Ezeket a sérüléseket eddig jóindulatúnak írták le, de a gyors orvosi beavatkozás mindig ajánlott.</p>
+"<h3>Klinikai viselkedés</h3>
+<p>Írisz-melanóma: egy jól körülhatárolt, sötét színű képlet jelenik meg az íriszen.</p> 
+<p>Ciliaris test melanóma: sötét burjánzásként látható a szemben, torzíthatja a pupillát, és azon keresztül előemelkedhet.</p> 
+<p>Choroidális melanóma: a choroidát és a retinát érinti. Kezdetben nem tapasztalhatók látáskárosodásra vagy gyulladásra utaló jelek. Később azonban szövődmények jelentkezhetnek, mint például vakság, glaukóma, retina leválás.</p>
+<p>A szem környékén megjelenő melanómáknál előfordulhat a szaruhártya helyi reakciója, fokozott könnytermelés és enyhe kötőhártya-irritáció. Ezeket a sérüléseket eddig jóindulatúnak írták le, de a gyors orvosi beavatkozás mindig ajánlott.</p>
 
 <h3>Gyakoriság</h3>
 <p>A szem környékén lévő melanómák főként 5–6 éves kutyákban fordulnak elő, míg a szem belső részében kialakuló melanómák általában 7 éves kortól jelentkeznek.</p>
@@ -25,7 +25,7 @@ $lang["cancer_tumorType_melanomaEye_characteristics"] =
 <p>A szem környéki melanómák leggyakrabban német juhász kutyáknál fordulnak elő.</p>";
 
 $lang["cancer_tumorType_melanomaEye_diagnosis"] = 
-"<p>Elsődlegesen alapos szemvizsgálat szükséges, amely magában foglalhatja a réslámpás mikroszkópos vizsgálatot, gonioscopiát, tonometriát, fundusvizsgálatot és/vagy szem ultrahangot. 
+"<p>Elsődlegesen alapos szemvizsgálat szükséges, amely magában foglalhatja a réslámpás mikroszkópos vizsgálatot, gonioszkópiát, tonometriát, fundusvizsgálatot és/vagy szem ultrahangot. 
 A daganat finomtű-aspirációja szóba jöhet a végleges diagnózis felállításához, de fertőzés, gyulladás és/vagy vérzés kockázatával jár.</p>";
 
 $lang["cancer_tumorType_melanomaEye_treatment"] = 
@@ -42,3 +42,4 @@ $lang["cancer_tumorType_melanomaEye_references"] =
 </ol>";
 
 ?>
+
