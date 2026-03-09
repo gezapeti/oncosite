6ba7d7c60bbf04edff0d835a651b0818ddc8cb71
gezapeti <2914398+gezapeti@users.noreply.github.com>
2026-03-09 12:01:40 +0100

    Update cancer_tumorType_melanomaNailbed_lang.php

--- src/cancer_tumorType_melanomaNailbed_lang.php
+++ src/cancer_tumorType_melanomaNailbed_lang.php
@@ -1,51 +1,51 @@
 <? 
 
-$lang["cancer_tumorType_melanomaNailbed_summary_location"] = "Lábujjak / talp";
-$lang["cancer_tumorType_melanomaNailbed_summary_behavior"] = "Az szájüregi és bőrmelanómák között helyezkedik el, gyakori áttétek (> 50%)";
+$lang["cancer_tumorType_melanomaNailbed_summary_location"] = "Lábujjak / talppárna";
+$lang["cancer_tumorType_melanomaNailbed_summary_behavior"] = "Az szájüregi és bőrmelanómák között helyezkedik el rosszindulatúság tekintetében, áttétképzés gyakori (> 50%)";
 $lang["cancer_tumorType_melanomaNailbed_summary_diagnosis"] = "Tűaspiráció / nyirokcsomó eltávolítás, röntgen (csont és mellüreg)";
 $lang["cancer_tumorType_melanomaNailbed_summary_treatment"] = "Primer daganat: sebészet és szükség esetén kiegészítő sugárkezelés";
 $lang["cancer_tumorType_melanomaNailbed_summary_prognosis"] = "Erősen függ a stádiumtól és a választott kezelési módtól";
 
 $lang["cancer_tumorType_melanomaNailbed_location"] = 
-"<p>Ez a melanóma a lábujjakon vagy a talpon fordulhat elő.</p> 
-<p>Melanómák kialakulhatnak a szőrös bőrön, a szemben, a szájüregben, a gyomor-bélrendszerben, valamint a nyálkahártya és a bőr átmeneti területein (pl. ajkak, végbél). A daganat helyétől függően a melanómák kevésbé vagy súlyosabban rosszindulatú viselkedést mutathatnak.</p>";
+"<p>Ez a melanóma a lábujjakon vagy a talppárnákon fordulhat elő.</p> 
+<p>Melanómák kialakulhatnak a szőrös bőrön, a szemben, a szájüregben, a gyomor-bélrendszerben, valamint a nyálkahártya és a bőr átmeneti területein (pl. ajkak, végbél). A daganat helyétől függően a melanómák kevésbé vagy erősen rosszindulatú viselkedést mutathatnak.</p>";
 
 $lang["cancer_tumorType_melanomaNailbed_characteristics"] = 
-"<h3>Viselkedés</h3>
+"<h3>Klinikai viselkedés</h3>
 <p>Rosszindulatú viselkedés szempontjából ez a daganattípus a szájüregi és a bőrmelanómák közé esik. A lokális nyirokcsomókba és a tüdőbe történő áttétek az esetek 58–71%-ában fordulnak elő, és a lábujjak melanómáinak 5%-a a csontok károsodásához vezethet.</p>
 
-<h3>Kinézet</h3>
-<p>Ezek a melanómák a körömágyban vagy a talpon nőhetnek. Különböző formákat ölthetnek: kis növedék, rosszul körülhatárolt duzzanat az érintett lábujjon, fekélyesedett növedék.</p>
+<h3>Megjelenés</h3>
+<p>Ezek a melanómák a körömágyban vagy a talpon nőhetnek. Különböző formákat ölthetnek: kis méretű növedék, rosszul körülhatárolt duzzanat az érintett lábujjon, felfekélyesedett szövetszaporulat.</p>
 
 <h3>Tünetek</h3>
-<p>A lábujjak duzzanata gyakran az első észlelhető tünet. Egyes kutyák elveszítik a körmüket vagy sántítanak az érintett lábon. Általában ezek a daganatok fekete színűek.</p>";
+<p>A lábujjak duzzanata gyakran az első észlelhető tünet. Egyes kutyák elveszítik a körmüket vagy sántítanak az érintett lábon. Sok esetben ezek a daganatok fekete színűek.</p>";
 
 $lang["cancer_tumorType_melanomaNailbed_diagnosis"] = 
-"<p>Finomtű-aspirációt kell végezni a daganatról, valamint a lokális nyirokcsomókról.</p> 
-<p>Röntgenfelvételeket kell készíteni az érintett lábujjról a csont érintettségének felmérésére. A tüdőröntgen szintén fontos az áttétek jelenlétének felméréséhez.</p>";
+"<p>Vékonytű-aspiráció (citológia) szükséges a daganatból, valamint a lokális nyirokcsomókból.</p> 
+<p>Röntgenfelvételeket érdemes készíteni az érintett lábujjról a csont érintettségének felmérésére. A tüdőröntgen vagy CT segít az áttétek jelenlétének felderítésében.</p>";
 
 $lang["cancer_tumorType_melanomaNailbed_treatment"] = 
 "<h3>Sebészet</h3>
-<p>A helyi melanómák esetében a sebészet a legmegfelelőbb kezelés. Ha a melanóma a körömágyban van, előfordulhat, hogy az egész lábujjat el kell távolítani a csontkárosodás miatt. 
-A talpon kialakuló melanóma akár a teljes végtag amputációjához vezethet, mivel a teljes testsúly az érintett talp részre nehezedhet, és a talp eltávolítása a láb funkcióvesztéséhez vezet.</p>
-<p>Az egyes ujjak amputációjának funkcionális kimenetele kiváló, és a részleges lábamputációk (több ujjat érintő kimetszés) is jól tolerálhatók, jó funkcionális eredménnyel.</p> 
-<p>Azoknál a daganatoknál, amelyek széles kimetszésre nem alkalmasak, vagy ahol a kimetszés nem biztosítja a teljes histológiai határt, érdemes a sebészetet sugárkezeléssel vagy más kiegészítő terápiákkal kombinálni.</p>
+<p>A lokalizált melanómák esetében a sebészet az elsődleges kezelés. Ha a melanóma a karomágyban van, előfordulhat, hogy az egész lábujjat el kell távolítani a csontkárosodás miatt. 
+A talppárnán kialakuló melanóma akár a teljes végtag amputációjához vezethet, mivel a teljes testsúly a megmaradó talp részre nehezedhet, így a talp egy részének eltávolítása a láb funkcióvesztéséhez vezet.</p>
+<p>Az egyes ujjak amputációjával a normál funkcionális működés megmarad, a műtét kimenetele kiváló. A részleges lábamputációk (több ujjat érintő eltávolítás) is jól tolerálhatók alapvetően.</p> 
+<p>Azoknál a tumoroknál, amelyeknél széles kimetszés nem lehetséges, vagy ahol az eltávolítás nem biztosítható ép sebszélekkel, érdemes a sebészetet sugárkezeléssel vagy más kiegészítő terápiákkal kombinálni.</p>
 
 <h3>Sugárkezelés</h3>
 <p>A sugárkezelés hatékony alternatíva a teljes mértékben nem eltávolítható daganatok esetén. A melanómák általában jól reagálnak a nagy dózisban adott sugárkezelésre (hetente egyszer, négy alkalommal). 
-A kezelés lehetséges mellékhatása a körmök elvesztése vagy a talp lehámlása. A körmök és a talpak újranőnek, és hosszú távú problémát nem okoznak.</p>
+A kezelés lehetséges mellékhatása a körmök elvesztése vagy a talp leválása, hámlása. A körmök és a talppárnák újranőnek, így hosszú távon nem kell problémára számítani.</p>
 
 <h3>Kemoterápia</h3>
-<p>A kemoterápia látszólag nem játszik jelentős szerepet a rosszindulatú melanómás kutyák kezelésében. A karboplatin alkalmazása segíthet az áttétek terjedésének késleltetésében vagy megelőzésében, bár a legtöbb válasz rövid életű. Eddig nincs publikált tanulmány, amely bizonyítaná, hogy a kemoterápia hosszabb túlélést eredményez. 
-A standard kemoterápiás protokoll a karboplatin kombinációja sebészettel és/vagy sugárkezeléssel. A karboplatint 3 hetente intravénásan adják, összesen 4 kezelés során. A mellékhatások minimálisak.</p>
+<p>A kemoterápia látszólag nem játszik jelentős szerepet a rosszindulatú melanómás kutyák kezelésében. A karboplatin alkalmazása segíthet az áttétek terjedésének késleltetésében vagy megelőzésében, bár jeletős esetben a terápiás válasz rövid idejű. Eddig nincs publikált tanulmány, amely bizonyítaná, hogy a kemoterápia összességében hosszabb túlélést eredményez. 
+A standard kemoterápiás protokoll a karboplatin kombinációja sebészettel és/vagy sugárkezeléssel. A karboplatint 3 hetente intravénásan adják, legalább 4 alkalommal. A mellékhatások minimálisak.</p>
 
 <h3>Melanóma vakcina</h3>
-<p>A melanómák a pigmentációs enzimet, a tirozinázt expresszálják, és annak kifejeződése nő a daganat növekedésével. Ezért a tirozinázt célzó kezelések a melanómákat is célozzák. Az Oncept® egy terápiás rák vakcina, amely idegen (humán) tirozinázt termel. Ez a vakcina megtanítja a kutya immunrendszerét a melanóma sejtek felismerésére és megtámadására. 
-Ez a vakcina érdekes lehet kiegészítő kezelésként minimális maradványos daganat esetén (amikor a lehető legkevesebb daganatszövet maradt, pl. műtét után), és ez a megközelítés még vizsgálat alatt áll. A vakcinázás indikált lehet rosszindulatú daganatoknál és a nyirokcsomók érintettsége esetén.</p>";
+<p>A melanómák egy ún. pigmentációs enzimet, a tirozinázt expresszálják, és annak kifejeződése nő a daganat növekedésével. Ezért a tirozinázt célzó kezelések a melanómákat is célozzák. Az Oncept® egy terápiás rák vakcina, amely idegen (humán) tirozinázt termel. Ez a vakcina megtanítja a kutya immunrendszerét a melanóma sejtek felismerésére és megtámadására. 
+Ez a vakcina érdekes lehet kiegészítő kezelésként minimális maradványos daganat esetén (amikor a lehető legkevesebb daganatszövet maradt, pl. műtét után),  ez a megközelítés még vizsgálat alatt áll. A vakcina opcionális kezelés lehet rosszindulatú daganatoknál és a nyirokcsomók érintettsége esetén.</p>";
 
 $lang["cancer_tumorType_melanomaNailbed_prognosis"] = 
-"<p>A körömágy és a talp melanómáinak kb. 50%-a ad áttétet a test más részeire. Ha a mitotikus index <3, a daganat valószínűleg jóindulatúan viselkedik, és kisebb az áttétképződés kockázata. 
-A helyi kiújulás kezelése rosszabb prognózissal jár, mint az eredeti daganat kezelése. A helyi kiújulás esélye 14–30%. Az átlagos túlélési idő 12 hónap; statisztikailag az első év után átlagosan 42–66% él, a második év után 12–22%.</p>";
+"<p>A körömágy és a talp melanómáinak kb. 50%-a ad áttétet a test más részeire. Ha a szövettannal megállapítható mitotikus index <3 ( /10 látótér 400x nagyításon), a daganat valószínűleg jóindulatúan viselkedik, és kisebb az áttétképződés kockázata. 
+A helyileg már kiújult tumor kezelése nehézkesebb, rosszabb prognózissal jár, mint az eredeti daganat kezelése. A helyi kiújulás esélye 14–30%. Az átlagos túlélési idő 12 hónap; statisztikailag az első év után átlagosan 42–66% él, a második év után 12–22%.</p>";
 
 $lang["cancer_tumorType_melanomaNailbed_references"] = 
 "<ol>
@@ -55,3 +55,4 @@ $lang["cancer_tumorType_melanomaNailbed_references"] =
 </ol>";
 
 ?>
+
